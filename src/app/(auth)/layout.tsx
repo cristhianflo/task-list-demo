3c4501eebb9d5b3025d6a57ecd2ae56ab1625744
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2025-12-25 16:32:24 -0400

    fix: removed redirecting in GuestLayout and AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,7 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-
 export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-
-  if (!session) return redirect("/");
-
   return children;
 }
