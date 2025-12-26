a9a059ee175d498e1df02e5943d736e170189183
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2025-12-25 23:49:51 -0400

    feat: created basic jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/cypress/"],
+  clearMocks: true,
+};
+
+export default config;
