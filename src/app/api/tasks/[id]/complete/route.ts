70b199a04f7a894c3d09a042dc79b9566f0f4b22
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2025-12-25 21:18:29 -0400

    feat: created api endpoint to send request to lambda function to mark task as done

---
+++ src/app/api/tasks/[id]/complete/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { setTaskPending } from "@/services/taskService";
+import { CompleteTaskParamsSchema } from "@/schemas/task";
+
+const lambdaClient = new LambdaClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+export async function POST(
+  req: NextRequest,
+  context: { params: { id: string } | Promise<{ id: string }> },
+) {
+  const session = await auth();
+
+  if (!session || !session.user?.id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // 2. Validate Params
+  // Await params if you are on Next.js 15 (optional but good practice)
+  const params = await context.params;
+  const taskId = params.id;
+  const validation = CompleteTaskParamsSchema.safeParse({ taskId });
+
+  if (!validation.success) {
+    return NextResponse.json({ error: "Invalid Task ID" }, { status: 400 });
+  }
+  try {
+    await setTaskPending(taskId, session.user.id);
+
+    const command = new InvokeCommand({
+      FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
+      InvocationType: "Event",
+      Payload: JSON.stringify({
+        id: taskId,
+        userId: session.user.id,
+      }),
+    });
+
+    await lambdaClient.send(command);
+
+    return NextResponse.json({ success: true, status: "PENDING" });
+  } catch (error) {
+    console.error("API Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
